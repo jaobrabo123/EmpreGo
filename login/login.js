bb914798f3ac821f5dc711c264773fb549dce28c
exdrama23 <alecviniciusflamengo@gmail.com>
2025-01-30 20:53:04 -0300

    nome do commit

--- login/login.js
+++ login/login.js
@@ -1,4 +1,7 @@
-function esqueceu(){
+//ainda não tive tempo de mexer
+
+
+/*function esqueceu(){
     let email = prompt('Digite o seu email e iremos te mandar uma mensagem para você alterar sua senha!')
 
     if (email !== null) {
@@ -21,4 +24,18 @@ function mostrarsenha(){
       senhaInput.type = "password"; // Torna o campo invisível
       document.getElementById("olho").src =  "imagens/olhofechado.png"
     }
+}*/
+
+function validarSenha() {
+  const senha = document.getElementById("senha").value;
+  const confirmaSenha = document.getElementById("confirmaSenha").value;
+  const mensagemErro = document.getElementById("mensagemErro");
+
+  if (senha !== confirmaSenha) {
+      mensagemErro.style.display = "block"; // Exibe a mensagem de erro
+      return false; // Impede o envio do formulário
+  } else {
+      mensagemErro.style.display = "none"; // Esconde a mensagem de erro
+      return true; // Permite o envio do formulário
+  }
 }

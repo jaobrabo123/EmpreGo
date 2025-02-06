7aea261bd74e7b4313e58748add8db778a4d4366
exdrama23 <alecviniciusflamengo@gmail.com>
2025-02-06 00:21:00 -0300

    Modificações em andamento... por enquanto temos modificaloes no visual e  mais 3 imagens

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function carregar() {
-    
-}
 
+var swiper = new Swiper(".home-slider", {
+    loop: true, // Faz o slider girar continuamente
+    spaceBetween: 30, // Espaçamento entre slides
+    centeredSlides: true, // Centraliza os slides
+    autoplay: {
+        delay: 3000, // Tempo de troca entre imagens (3 segundos)
+        disableOnInteraction: false, // Continua mesmo após interação do usuário
+    },
+    pagination: {
+        el: ".swiper-pagination", // Ativa os bullets de navegação
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-next", // Botão próximo
+        prevEl: ".swiper-prev", // Botão anterior
+    },
+  });

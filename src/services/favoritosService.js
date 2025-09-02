730d9685fa321c735f7beacf670f4dc59429addf
jaobrabo123 <jp2904.lindo@gmail.com>
2025-09-01 23:00:50 -0300

    Limitação de 10 favoritos back

--- src/services/favoritosService.js
+++ src/services/favoritosService.js
@@ -1,9 +1,18 @@
 // * Prisma
 const prisma = require('../config/db.js');
+const Erros = require('../utils/erroClasses.js');
 
 class FavoritosService {
 
     static async candidatoFavoritarEmpresa(empresa, candidato){
+        const favoritadasCount = await prisma.favoritos_empresas.count({
+            where: {
+                id_candidato: candidato
+            }
+        });
+
+        if(favoritadasCount>=10) throw new Erros.ErroDeValidacao("Você não pode favoritar mais de 10 empresas.");
+
         await prisma.favoritos_empresas.create({
             data: {
                 cnpj_empresa: empresa,

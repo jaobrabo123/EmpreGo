536c4dc29866395cbbf39b25ecea6a368b1d78e7
jaobrabo123 <jp2904.lindo@gmail.com>
2025-09-12 18:25:51 -0300

    Resolvendo uns problemas do chat

--- public/js/notfSocketsConfig.js
+++ public/js/notfSocketsConfig.js
@@ -3,10 +3,4 @@ const server = window.location.hostname.includes('localhost') ?
 'http://localhost:3001' : 'https://tcc-vjhk.onrender.com';
 var socket = io(server);
 
-const intervalId = setInterval(() => socket.emit('refreshStatus'), 5000);
-
-window.addEventListener('beforeunload', () => {
-  clearInterval(intervalId);
-});
-
 export default socket;

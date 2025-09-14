91588ec7a34d1041d2b11ad8dd27d52f340ea797
jaobrabo123 <jp2904.lindo@gmail.com>
2025-09-14 15:22:29 -0300

    Mensagens sendo salvas em base64 para ocupar menos espaço

--- src/utils/cryptoMsgs.js
+++ src/utils/cryptoMsgs.js
@@ -6,16 +6,16 @@ const IV_SIZE = 16;
 
 function criptografarMensagem(mensagem){
     const iv = crypto.randomBytes(IV_SIZE);
-    const cifra = crypto.createCipheriv(ALGORITMO, Buffer.from(SECRET_KEY_CRYPTO, 'hex'), iv);
+    const cifra = crypto.createCipheriv(ALGORITMO, SECRET_KEY_CRYPTO, iv);
     const criptografado = Buffer.concat([cifra.update(mensagem, 'utf8'), cifra.final()]);
-    return `${iv.toString('hex')}:${criptografado.toString('hex')}`;
+    return `${iv.toString('base64')}:${criptografado.toString('base64')}`;
 }
 
 function descriptografarMensagem(mensagemCriptografada){
     const [ivHex, hexCriptografado] = mensagemCriptografada.split(':');
-    const iv = Buffer.from(ivHex, 'hex');
-    const textoCriptografado = Buffer.from(hexCriptografado, 'hex');
-    const decifrado = crypto.createDecipheriv(ALGORITMO, Buffer.from(SECRET_KEY_CRYPTO, 'hex'), iv);
+    const iv = Buffer.from(ivHex, 'base64');
+    const textoCriptografado = Buffer.from(hexCriptografado, 'base64');
+    const decifrado = crypto.createDecipheriv(ALGORITMO, SECRET_KEY_CRYPTO, iv);
     const descriptografato = Buffer.concat([decifrado.update(textoCriptografado), decifrado.final()]);
     return descriptografato.toString('utf8');
 }
